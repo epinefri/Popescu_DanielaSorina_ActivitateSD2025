97659af2e6a023533fe791d17a15012dbb350004
epinefri <danielasorinap@gmail.com>
2025-03-11 13:58:16 +0200

    Adaugat cerere introducere nr intreg

--- seminar01.c
+++ seminar01.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main() {
-	printf("Salut!\n");
+	printf("Salut!\n Introduceti un numar intreg: ");
+	int variabila = 0;
+	scanf_s("%d", &variabila);
+	printf("Ati introdus: %d", variabila);
 	return 0;
 }
